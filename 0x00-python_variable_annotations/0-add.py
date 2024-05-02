f49fdb1f7f6208fd548b5d88bd49a4a9ff17a73e
SOGs12 <mosesoyewumi05@gmail.com>
2024-05-02 16:13:30 -0700

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python3
+"""Type-annotated function add"""
+
 
 def add(a: float, b: float) -> float:
-    """
-    Add two float numbers and return the sum.
-
-    Args:
-        a: First float number.
-        b: Second float number.
-
-    Returns:
-        The sum of the two input numbers.
-    """
-    # Calculate the sum of a and b
+    """Takes two floats and returns their sum as float"""
     return a + b
-
-# For testing the function
-if __name__ == "__main__":
-    # Test the add function
-    result = add(1.11, 2.22)
-    print(result == 1.11 + 2.22)
-    print(add.__annotations__)
