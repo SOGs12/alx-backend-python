0fb18b2dd5ce2695bfd1f03ae554a81a95f2ed49
SOGs12 <mosesoyewumi05@gmail.com>
2024-05-02 17:24:19 -0700

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
-from typing import Tuple, Any, Union, Sequence,List
+"""
+Contains a function that returns  a list of integers
+multiplied by certain factor.
+"""
+from typing import Tuple, List
 
 
-def zoom_array(lst: Sequence[int], factor: Union[int, float] = 2) -> Tuple[Union[int, float]]:
-
-    if isinstance(factor, float):
-        factor = int(factor)
-    zoomed_in: List[Union[int, float]]= [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Returns a list of integers multiplied by certain factor.
+    Args:
+        lst: A tuple of integers.
+        factor: An integer.
+    Returns:
+        A list of integers.
+    """
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array: Tuple = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
